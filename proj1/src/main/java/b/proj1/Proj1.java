fe94c599620565fc821bc4a5b9834eaac52bae7d
S <s@gmail.com>
2024-08-05 08:41:56 -0500

    Se adiciona info

---
+++ proj1/src/main/java/b/proj1/Proj1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package b.proj1;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Proj1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
